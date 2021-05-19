73781b25474cc5a9dc610cd83c5671b201049100
sri-harshaa <84479660+sri-harshaa@users.noreply.github.com>
2021-05-19 16:51:04 -0700

    initial code for testbench top module

--- Testbench/top.sv
+++ Testbench/top.sv
@@ -1,1 +1,24 @@
+import AHBpkg::*;
+import uvm_pkg::*;
 
+module top();
+  
+  bit HCLK;
+  logic HRESETn;
+  
+  AHBInterface intf(HCLK,HRESETn);      //Interface handle
+  
+  AHBSlaveTop DUT (intf.slave);        //DUT instantiation
+  
+  always #10 HCLK = ~HCLK;
+  
+  initial
+    begin
+      uvm_config_db #(virtual AHBInterface) :: set (null,"*","interface",intf);
+      
+      run_test();
+      
+      $finish();
+  end
+endmodule
+  
