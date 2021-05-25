cf3d5c9822ddfdedfe3dc78342fa11f6a09c670f
shivanandgsr <gujjula@pdx.edu>
2021-05-24 18:54:54 -0700

    update2
    for virtual sequence and sequencer

--- Testbench/AHB_virtual_sequence.sv
+++ Testbench/AHB_virtual_sequence.sv
@@ -1,6 +1,11 @@
+import AHBpkg::*;
+import uvm_pkg::*;
+`include "uvm_macros.svh"
+`include "AHB_sequences.sv"
+`include "AHB_virtual_sequencer.sv"
 
 class AHB_virtual_sequence extends uvm_sequence;
-	`uvm_object_utilis(AHB_virtual_sequence)
+	`uvm_object_utils(AHB_virtual_sequence)
 	`uvm_declare_p_sequencer(AHB_virtual_sequencer)
 	
 	function new(string name = "AHB_virtual_sequence");
@@ -17,14 +22,14 @@ class AHB_virtual_sequence extends uvm_sequence;
 	sequence_WRAP16_burst seq_wrap16;
 	
 	task prebody();
-		seq_single = sequence_SINGLE_burst::typeid::create("seq_single");
-		seq_incr   = sequence_INCR_burst  ::typeid::create("seq_incr");
-		seq_incr4  = sequence_INCR4_burst ::typeid::create("seq_incr4");
-		seq_incr8  = sequence_INCR8_burst ::typeid::create("seq_incr8");
-		seq_incr16 = sequence_INCR16_burst::typeid::create("seq_incr16");
-		seq_wrap4  = sequence_WRAP4_burst ::typeid::create("seq_wrap4");
-		seq_wrap8  = sequence_WRAP8_burst ::typeid::create("seq_wrap8");
-		seq_wrap16 = sequence_WRAP16_burst::typeid::create("seq_wrap16");
+		seq_single = sequence_SINGLE_burst::type_id::create("seq_single");
+		seq_incr   = sequence_INCR_burst  ::type_id::create("seq_incr");
+		seq_incr4  = sequence_INCR4_burst ::type_id::create("seq_incr4");
+		seq_incr8  = sequence_INCR8_burst ::type_id::create("seq_incr8");
+		seq_incr16 = sequence_INCR16_burst::type_id::create("seq_incr16");
+		seq_wrap4  = sequence_WRAP4_burst ::type_id::create("seq_wrap4");
+		seq_wrap8  = sequence_WRAP8_burst ::type_id::create("seq_wrap8");
+		seq_wrap16 = sequence_WRAP16_burst::type_id::create("seq_wrap16");
 	endtask
 	
 	task body();
--- Testbench/AHB_virtual_sequencer.sv
+++ Testbench/AHB_virtual_sequencer.sv
@@ -1,7 +1,10 @@
+import AHBpkg::*;
+import uvm_pkg::*;
+`include "uvm_macros.svh"
 
 class AHB_virtual_sequencer extends uvm_sequencer;
 
-	`uvm_component_utilis(AHB_virtual_sequencer)
+	`uvm_component_utils(AHB_virtual_sequencer)
 	
 	function new(string name = "AHB_virtual_sequencer", string parent = "uvm_component");
 		super.new(name,parent);
