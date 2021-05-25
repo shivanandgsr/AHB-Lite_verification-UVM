a91a9170cdbbb042fa67be83f40d124f93ba7d69
shivanandgsr <gujjula@pdx.edu>
2021-05-25 11:34:00 -0700

    Update AHB_Interface.sv

--- Testbench/AHB_Interface.sv
+++ Testbench/AHB_Interface.sv
@@ -3,18 +3,18 @@ import AHBpkg::*;
 
 interface AHB_Interface (input bit HCLK, input bit HRESETn);
 
-	logic  	HRESP;
-	logic	HWRITE;
-	logic	HREADY;
-	logic  	[ADDRWIDTH-1:0] HADDR;
-	logic  	[DATAWIDTH-1:0] HWDATA;
-	logic	[DATAWIDTH-1:0] HRDATA;
+        logic HREADY;
+		logic HWRITE;
+		logic [1:0]  HRESP;
+        logic [1:0]  HTRANS;
+        logic [2:0]  HBURST;
+        logic [2:0]  HSIZE;
+        logic [31:0] HADDR;
+        logic [31:0] HWDATA;
+		logic [31:0] HRDATA;
 
-	HSIZE_TYPE	HSIZE;
-	HBURST_TYPE HBURST;
-	HTRANS_TYPE HTRANS;
-	
-	modport DUT (input 	HCLK,
+
+		modport DUT (input 	HCLK,
 						HRESETn,
 						HADDR,
 						HWDATA,
@@ -22,9 +22,40 @@ interface AHB_Interface (input bit HCLK, input bit HRESETn);
 						HBURST,
 						HTRANS
 				
-				output	HRESP,
+					output	HRESP,
 						HREADY,
 						HRDATA
-				);
-  
+					);
+				
+		clocking drv_cb@(posedge HCLK);
+			default input #1 output #0;
+			output 	HTRANS,
+					HBURST,
+					HWRITE,
+					HWDATA,
+					HSIZE,
+					HADDR;
+			
+			input 	HREADY,
+					HRDATA,
+					HRESP;
+
+        endclocking
+       
+		clocking mon_cb@(posedge HCLK);
+			default input #1 output #0;
+
+			input 	HRESETn,
+					HTRANS,
+					HBURST,
+					HWRITE,
+					HWDATA,
+					HREADY,
+					HRDATA,
+					HSIZE,
+					HADDR,
+					HRESP;
+
+        endclocking
+
 endinterface
